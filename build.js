d2add7c75e083615a40d0fcf81f5fb0e041e6ef5
nemuio-o <nemu1.2500.o.o@gmail.com>
2026-02-01 22:53:43 +0900

    Create build.js

---
+++ build.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+const html = fs.readFileSync('src/index.html', 'utf8')
+const js = fs.readFileSync('src/app.js', 'utf8')
+let worker = fs.readFileSync('src/index.ts', 'utf8')
+
+worker = worker.replace('__HTML_CONTENT__', html.replace(/`/g, '\\`'))
+worker = worker.replace('__JS_CONTENT__', js.replace(/`/g, '\\`'))
+
+fs.mkdirSync('dist', { recursive: true })
+fs.writeFileSync('dist/index.ts', worker)
+console.log('✅ Build complete!')
